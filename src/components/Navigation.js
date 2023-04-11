c248584a63ca27137425cbf0e0e236df27db6a5c
David Keim <dmk252@cornell.edu>
2023-04-11 18:49:21 -0400

    formatting

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,25 +6,25 @@ function Navigation({ currentPage, handlePageChange }) {
             <a
                 href="#about-me"
                 onClick={() => handlePageChange('about-me')}
-                className={currentPage === 'about-me' ? 'bg-primary text-white' : 'nav-link'}
+                className={currentPage === 'about-me' ? 'bg-primary text-white w-25  text-center' : 'w-25 text-center'}
             >About Me
             </a>
             <a
                 href="#portfolio"
                 onClick={() => handlePageChange('portfolio')}
-                className={currentPage === 'portfolio' ? 'bg-primary text-white' : 'nav-link'}
+                className={currentPage === 'portfolio' ? 'bg-primary text-white w-25 text-center' : 'w-25 text-center'}
             >Portfolio
             </a>
             <a
                 href="#resume"
                 onClick={() => handlePageChange('resume')}
-                className={currentPage === 'resume' ? 'bg-primary text-white' : 'nav-link'}
+                className={currentPage === 'resume' ? 'bg-primary text-white w-25 text-center' : 'w-25 text-center'}
             >Resume
             </a>
             <a
                 href="#contact"
                 onClick={() => handlePageChange('contact')}
-                className={currentPage === 'contact' ? 'bg-primary text-white' : 'nav-link'}
+                className={currentPage === 'contact' ? 'bg-primary text-white w-25 text-center' : 'w-25 text-center'}
             >Contact
             </a>
         </div>
