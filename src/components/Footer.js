41e0ef91381c57d7ae51e09a53afed926b15b88a
David Keim <dmk252@cornell.edu>
2023-04-11 19:06:34 -0400

    updatedd aobut formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div className="d-flex flex-row justify-content-around">
+    <div className="d-flex flex-row justify-content-around bg-primary text-white">
       <p>GitHub Link</p>
       <p>LinkedIn Link</p>
     </div>
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,10 +4,13 @@ import pic from "../../assets/profile-pic.png";
 function About() {
   return (
       <div className="d-flex flex-column align-items-center">
-        <p>About</p>
+        <h3>About</h3>
         <img className="w-50" src={pic} alt="Profile"></img>
         <p className="text-center">
-          Full stack web developer with background in chemical engineering and management consulting
+          Full stack web developer with background in chemical engineering and management consulting.
+        </p>
+        <p className="text-center">
+          Interested in designing games, music, and good food.
         </p>
       </div>
   );
