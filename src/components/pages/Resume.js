dd3da96c75880e2181ea9b0f1945291b64bb723d
David Keim <dmk252@cornell.edu>
2023-04-12 19:53:01 -0400

    changes

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 function Resume() {
   return (
-      <div className="d-flex flex-row justify-content-center">
-        <p>Resume</p>
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <h3>Resume</h3>
+        <object data="../../assets/david-keim-resume.pdf" type="application/pdf" width="100%" height="500px"></object>
       </div>
   );
 }
