6e81354f6311c35a6bd5967d535666302bef0816
David Keim <dmk252@cornell.edu>
2023-04-12 20:13:58 -0400

    test

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,8 +4,8 @@ function Resume() {
   return (
       <div className="d-flex flex-column justify-content-center align-items-center">
         <h3>Resume</h3>
-        <object data="../../react-portfolio-challenge/tree/main/src/assets/david-keim-resume.pdf" type="application/pdf" width="100%" height="500px">
-        <p>Alternative text - include a link <a href="../../react-portfolio-challenge/tree/main/src/assets/david-keim-resume.pdf">to the PDF!</a></p>
+        <object data="../../react-portfolio-challenge/raw/main/src/assets/david-keim-resume.pdf" type="application/pdf" width="100%" height="500px">
+        <p>Alternative text - include a link <a href="../../react-portfolio-challenge/raw/main/src/assets/david-keim-resume.pdf">to the PDF!</a></p>
         </object>
       </div>
   );
